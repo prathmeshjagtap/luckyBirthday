0e712999d4c64594f34c181d8d3eb843fde99ad7
prathmesh <prathmeshjagtap405@gmail.com>
2021-07-25 15:09:49 +0530

    feat

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ const birthDate = document.getElementById("birthDate");
 const luckyNumber = document.getElementById("luckyNumber");
 const output = document.getElementById("output");
 var image = document.getElementById("image");
+const form = document.getElementById("form");
 
-function check() {
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
   var sum = 0;
+  const birthValue = birthDate.value;
+  const luckyValue = luckyNumber.value;
 
-  const birth = birthDate.value.split("-").join("");
+  const birth = birthValue.split("-").join("");
   for (const i in birth) {
     sum = sum + birth[i];
   }
-  if (sum % luckyNumber.value === 0) {
+  if (sum % luckyValue === 0) {
     output.textContent = "hooray ! you are a Lucky Person ";
-    image.src = "./data/do-you-even-know-how-lucky-you-are.jpg";
+    image.src = "./icons/do-you-even-know-how-lucky-you-are.jpg";
   } else {
     output.textContent = "Oops!!Your birthday is not a lucky number!";
-    image.src = "./data/oh-no-seems-like-you-are-not-that-lucky-today.jpg";
+    image.src = "./icons/oh-no-seems-like-you-are-not-that-lucky-today.jpg";
   }
-}
+});
